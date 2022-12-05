ed35d7e6baa38fc4e247d98ced8f3eebe7009115
hikal <hikal@DESKTOP-5C113OV>
2022-12-05 21:46:56 +0200

    Add project files.

---
+++ Portal.DAL/DataBase/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portal.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.DAL.DataBase
+{
+    internal class ApplicationDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=Portal;Integrated Security=True");
+        }
+        public DbSet<Department> Department { get; set; }
+
+    }
+}
---
+++ Portal.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portal.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Department()
+        {
+          
+            return View();
+
+        }
+    }
+}
